550cd6e95f40b1b4761205cd4004f041f90cad13
inspire12 <ox4443@naver.com>
2025-12-02 16:22:06 +0900

    feat: upstage chat api 추가

--- main.py
+++ main.py
@@ -21,3 +21,26 @@ def query(message:str):
     )
 
     print(response.data[0].embedding)
+
+@app.post("/chat")
+def query(message: str):
+    from openai import OpenAI  # openai==1.52.2
+
+    client = OpenAI(
+        api_key="up_CQlRUTuZcA4l3YwnD7g9dAdT74hXw",
+        base_url="https://api.upstage.ai/v1"
+    )
+    stream = client.chat.completions.create(
+        model="solar-pro2",
+        messages=[
+            {
+                "role": "user",
+                "content": message
+            }
+        ],
+        stream=False,
+    )
+    return stream.choices[0].message.content
+
+    # Use with stream=False
+    # print(stream.choices[0].message.content)
